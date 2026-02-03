02d62165554a95e49b669d72296824895a2b9746
Metty1337 <c137morty3@gmail.com>
2026-02-03 13:15:19 +0300

    refactor: rewrite AuthController for matching RPG pattern

--- src/main/java/metty1337/controller/AuthController.java
+++ src/main/java/metty1337/controller/AuthController.java
@@ -16,11 +16,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/auth")
@@ -28,6 +30,8 @@ public class AuthController {
 
   private static final Logger log = LoggerFactory.getLogger(AuthController.class);
   private static final String AUTH_TOKEN_COOKIE_NAME = "AUTH_TOKEN";
+  private static final String SIGN_UP_FORM_DTO_ATTR = "signUpFormDto";
+  private static final String SIGN_IN_FORM_DTO_ATTR = "signInFormDto";
   private final long TIME_TO_COOKIE_LIVE;
   private final AuthService authService;
   private final SessionService sessionService;
@@ -45,19 +49,26 @@ public class AuthController {
   }
 
   @GetMapping("/signup")
-  public String signUpPage(SignUpFormDto signUpFormDto) {
+  public String signUpPage(Model model) {
+    if (!model.containsAttribute(SIGN_UP_FORM_DTO_ATTR)) {
+      model.addAttribute(SIGN_UP_FORM_DTO_ATTR, new SignUpFormDto());
+    }
     return "sign-up";
   }
 
   @PostMapping("/signup")
   public String signUp(
-      @Valid @ModelAttribute("signUpFormDto") SignUpFormDto signUpFormDto,
-      BindingResult bindingResult) {
+      @Valid @ModelAttribute(SIGN_UP_FORM_DTO_ATTR) SignUpFormDto signUpFormDto,
+      BindingResult bindingResult, RedirectAttributes redirectAttributes) {
     log.debug("Attempting registration, username={}", signUpFormDto.getUsername());
 
     if (bindingResult.hasErrors()) {
       log.warn("Registration form has errors, username={}", signUpFormDto.getUsername());
-      return "sign-up-with-errors";
+      redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + SIGN_UP_FORM_DTO_ATTR,
+          bindingResult);
+      redirectAttributes.addFlashAttribute(SIGN_UP_FORM_DTO_ATTR, signUpFormDto);
+
+      return "redirect:/auth/signup-with-errors";
     }
     User user = userService.createUser(signUpFormDto);
     log.info("User registered, userId={}", user.getId());
@@ -65,21 +76,35 @@ public class AuthController {
     return "redirect:/auth/signin";
   }
 
+  @GetMapping("/signup-with-errors")
+  public String signUpWithErrorsPage(Model model) {
+    if (!model.containsAttribute(SIGN_UP_FORM_DTO_ATTR)) {
+      model.addAttribute(SIGN_UP_FORM_DTO_ATTR, new SignUpFormDto());
+    }
+    return "sign-up-with-errors";
+  }
+
   @GetMapping("/signin")
-  public String signInPage(SignInFormDto signInFormDto) {
+  public String signInPage(Model model) {
+    if (!model.containsAttribute(SIGN_IN_FORM_DTO_ATTR)) {
+      model.addAttribute(SIGN_IN_FORM_DTO_ATTR, new SignInFormDto());
+    }
     return "sign-in";
   }
 
   @PostMapping("/signin")
   public String signIn(
-      @Valid @ModelAttribute("signInFormDto") SignInFormDto signInFormDto,
+      @Valid @ModelAttribute(SIGN_IN_FORM_DTO_ATTR) SignInFormDto signInFormDto,
       BindingResult bindingResult,
-      HttpServletResponse response) {
+      HttpServletResponse response, RedirectAttributes redirectAttributes) {
     log.debug("Attempting login, username={}", signInFormDto.getUsername());
 
     if (bindingResult.hasErrors()) {
       log.warn("Login form has errors, username={}", signInFormDto.getUsername());
-      return "sign-in-with-errors";
+      redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + SIGN_IN_FORM_DTO_ATTR,
+          bindingResult);
+      redirectAttributes.addFlashAttribute(SIGN_IN_FORM_DTO_ATTR, signInFormDto);
+      return "redirect:/auth/signin-with-errors";
     }
     String token = authService.authenticate(signInFormDto);
 
@@ -89,6 +114,14 @@ public class AuthController {
     log.info("User logged in, username={}", signInFormDto.getUsername());
 
     return "redirect:/index";
+  }
+
+  @GetMapping("/signin-with-errors")
+  public String signInWithErrors(Model model) {
+    if (!model.containsAttribute(SIGN_IN_FORM_DTO_ATTR)) {
+      model.addAttribute(SIGN_IN_FORM_DTO_ATTR, new SignInFormDto());
+    }
+    return "sign-in-with-errors";
   }
 
   @PostMapping("/logout")
