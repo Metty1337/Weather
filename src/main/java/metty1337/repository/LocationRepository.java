2546ec7abee937b1d5cee5e2974f10a013beb4c6
Metty1337 <c137morty3@gmail.com>
2026-02-11 14:26:28 +0300

    refactor(LocationRepository): delete unused method

--- src/main/java/metty1337/repository/LocationRepository.java
+++ src/main/java/metty1337/repository/LocationRepository.java
@@ -3,7 +3,6 @@ package metty1337.repository;
 import java.math.BigDecimal;
 import java.util.List;
 import metty1337.entity.Location;
-import metty1337.entity.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -14,7 +13,5 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
 
   void deleteByLatitudeAndLongitudeAndUserId(BigDecimal latitude, BigDecimal longitude,
       Long userId);
-
-  Long user(User user);
 }
 
